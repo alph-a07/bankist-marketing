be4a4b44887403829475aee29fd0547d4793f1b4
Jeel <itsjeel01@gmail.com>
2024-01-02 10:20:21 +0530

    Implemented smooth navigation

--- script.js
+++ script.js
@@ -4,6 +4,10 @@ const modal = document.querySelector('.modal');
 const overlay = document.querySelector('.overlay');
 const btnCloseModal = document.querySelector('.btn--close-modal');
 const btnsOpenModal = document.querySelectorAll('.btn--show-modal');
+const btnScrollTo = document.querySelector('.btn--scroll-to');
+const section1 = document.getElementById('section--1');
+
+// ------------ FUNCTIONS ------------
 
 //-> Function to display modal
 const openModal = function (e) {
@@ -19,6 +23,18 @@ const closeModal = function () {
     overlay.classList.add('hidden');
 };
 
+// PAGE NAVIGATION
+document.querySelector('.nav__links').addEventListener('click', function (event) {
+    event.preventDefault();
+
+    if (event.target.classList.contains('nav__link')) {
+        const sectionId = event.target.getAttribute('href');
+        document.querySelector(sectionId).scrollIntoView({ behavior: 'smooth' });
+    }
+});
+
+// ------------ EVENT LISTENERS ------------
+
 //-> Open modal event listener
 btnsOpenModal.forEach(btn => {
     btn.addEventListener('click', openModal);
@@ -32,4 +48,12 @@ document.addEventListener('keydown', function (e) {
     if (e.key === 'Escape' && !modal.classList.contains('hidden')) {
         closeModal();
     }
+});
+
+//-> Learn more event listener
+btnScrollTo.addEventListener('click', function () {
+    const s1coords = section1.getBoundingClientRect();
+    console.log(s1coords.left, s1coords.top);
+
+    section1.scrollIntoView({ behavior: 'smooth' });
 });
